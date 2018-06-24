abf5860096f429bf62b7ab36ac9b986a33a700c7
Saravanan S <ssaravananerd@gmail.com>
2018-06-24 23:15:53 +0530

    AngularJS - Part 4 Demo Source
    Commiting AngularJS - Part 4 Demo source codes

---
+++ AngularJSPart4/AngularJSPart4/App/controllers/controllers.js
@@ -0,0 +1,63 @@
+﻿ssApp.controller("ssPart4Controller",
+["$scope", "$http",
+function ($scope, $http) {
+
+    $rootScope.name = "user name";
+
+    $scope.model = {};
+
+    $rootScope.model = $scope.model;
+
+    $rootScope.model.rootname = "root values";
+
+    $scope.models = [];
+
+    $scope.currencyvalue = 1234567890.1234;
+
+    $scope.datevalue = new Date();
+
+    $scope.jsonvalue = { FirstName: 'Saravanan', LastName: 'Somasundaram', Position: 'Project Leader', Experties: 'AngularJS, Angular 2, Angular 4 & Angular 5', newpro: "testing property" };
+
+    $scope.startIndex = 1;
+    $scope.limit = 5;
+
+    $scope.lowercasevalue = "SaMpLE LOWER CASE text";
+
+    $scope.numbervalue = 12345.6789;
+
+    $scope.uppercasevalue = "SaMpLE upper CASE text";
+
+    $scope.initialze = function () {
+        LoadData();
+    }
+
+    function LoadData() {
+        $http.get("api/sample/getdata").then(function (response) {
+            $scope.models = response.data;
+        },
+        function (error, code) {
+            console.log(error);
+        });
+    }
+
+    $scope.PostData = function () {
+        $http.post("api/sample/post",$scope.models).then(function (response) {
+            console.log(response);
+        },
+        function (error, code) {
+            console.log(error);
+        });
+    }
+
+}]);
+
+ssApp.controller("ssPart4_1Controller",
+["$scope", "$rootScope",
+function ($scope, $rootScope) {
+
+    $scope.model = $rootScope.model;
+
+    $scope.initialze = function () {
+
+    }
+}])
---
+++ AngularJSPart4/AngularJSPart4/Controllers/SampleController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AngularJSPart1.Controllers
+{
+    [RoutePrefix("api/sample")]
+    public class SampleController : ApiController
+    {
+        [Route("getdata")]
+        [HttpGet]
+        public IHttpActionResult GetData()
+        {
+            string[] data = {"Lucy Barletta","Louella Bun","Conrad Blish","Alvina Speck","Elia Sarabia","Galen Tolan","Nisha Amin","Keenan Justice",
+                "Mireya Alongi","Roni Gabler","Lilliam Eagle","Samatha Vannostrand","Tanner Tisby","Kanesha Puchalski","Carmelita Shute","Damien Winslow",
+                "Roberta Dunkerson","Chance Portalatin","Boris Zak","Loralee Marciniak","Annelle Mcdonell","Carissa Buresh","Luisa Schipper","Jacqualine Gresham",
+                "Adam Pal","Hyman Cosey","Katy Solorzano","Novella Owusu","Rozanne Mccary","Takisha Desantis","Vivian Loch","Shery Lichty","Ernestina Sullivan",
+                "Melony Trowell","Demetra Kellison","Oralee Motz","Yajaira Wadlington","Jesica Pop","Wava Pea","An Eyler","Porfirio Brittan","Zane Vantrease",
+                "Domonique Jeanbart","Judson Decicco","Mauro Timm","Ethel Shedd","Violette Elsass","Cleotilde Cardiel","Dollie Groll","Diane Jensen" };
+            return Ok(data);
+        }
+
+        [Route("post")]
+        [HttpPost]
+        public IHttpActionResult Post(string[] data)
+        {
+            if (data != null)
+            {
+                return Ok("Data received");
+            }
+            else
+            {
+                return BadRequest("Data not received");
+            }
+        }
+    }
+}
